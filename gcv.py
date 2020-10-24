69060400594623455942e4aba60c63d13169fe65
iyasha <mr.robot0n@gmail.com>
2020-10-24 18:04:09 +0300

    v0.2.0 * Добавил 3 свойства для получения последнего коммита, последней версии из коммита, и версию коммита

--- gcv.py
+++ gcv.py
@@ -1,7 +1,11 @@
 from git import Repo
+import git.objects.commit as goc
 from typing import *
 from git.exc import InvalidGitRepositoryError
 from output_formatters import ExceptionFormatter
+import re
+
+VERSION_TEMPLATE = 'v$major.$minor.$patch*$message'
 
 
 class GCV:
@@ -10,19 +14,29 @@ class GCV:
         try:
             self.repository = Repo(path)
         except InvalidGitRepositoryError:
-            ExceptionFormatter.format(f'Repository not found in path: {path}', 'Please, try in another folder with .git')
+            ExceptionFormatter.format(f'Repository not found in path: {path}',
+                                      'Please, try in another folder with .git')
         self.message = message
-        self.version = version
+        self.version = version if version is not None else self._get_version_
 
-    def get_version(self):
-        pass
+    @property
+    def get_last_commit(self) -> Optional[goc.Commit]:
+        return next(self.repository.iter_commits())
+
+    @property
+    def _get_version_(self) -> str:
+        commit = self.get_last_commit
+        if commit is not None:
+            pattern = re.escape(VERSION_TEMPLATE)
+            pattern = re.sub(r'\\\$(\w+)', r'(?P<\1>.*)', pattern)
+            match = re.match(pattern, commit.message)
+            if match is not None:
+                return f'v{match.group("major").strip()}.{match.group("minor").strip()}.{match.group("patch").strip()}'
+        return 'v0.1.0'
+
+    @property
+    def version(self):
+        return self.version
 
     def commit(self):
         pass
-
-
-    # repo = Repo(path)
-    # assert not repo.bare
-    # for commit in repo.iter_commits():
-    #     print(commit.message)
-    #     print(commit.author)
